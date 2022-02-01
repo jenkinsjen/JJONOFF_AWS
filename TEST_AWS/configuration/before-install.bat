e7de674bd48f9fe84873e066605a65fc0b971612
jenkinsjen <78421093+jenkinsjen@users.noreply.github.com>
2022-02-01 10:42:19 -0500

    Update before-install.bat

--- TEST_AWS/configuration/before-install.bat
+++ TEST_AWS/configuration/before-install.bat
@@ -1,4 +1,4 @@
 del "c:\inetpub\wwwroot\configuration\ApplicationConfig.txt"
 Copy "c:\inetpub\wwwroot\Web.config" "c:\inetpub\wwwroot\configuration\ApplicationConfig.txt"
-Copy "c:\inetpub\wwwroot\configuration\MaintenanceConfigFile.text" "c:\inetpub\wwwroot\Web.Config"
+Copy "c:\inetpub\wwwroot\configuration\MaintenanceConfigFile.txt" "c:\inetpub\wwwroot\Web.Config"
 tar.exe -a -c -f c:/Jenkins/dev_Source_backup_%time:~0,2%%time:~3,2%%time:~6,2%_%date:~-10,2%%date:~-7,2%%date:~-4,4%.zip c:/inetpub/wwwroot/**
